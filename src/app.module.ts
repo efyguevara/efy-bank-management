8beacd480af8c51267fc1ccaf7a6cec956eb47ad
Stefany Guevara <sguevara@cleveritgroup.com>
2023-03-11 23:18:42 +0100

    Database configuration and add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import config from '../config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+      }),
+    }),
+    DatabaseModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
