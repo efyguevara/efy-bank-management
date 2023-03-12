5be8e0a1ec6f1791ab92e9f51da3cebbf1cf4e6a
Stefany Guevara <sguevara@cleveritgroup.com>
2023-03-12 13:03:23 +0100

    Validations to send the correct parameters to user requests

--- src/user/dtos/users.dto.ts
+++ src/user/dtos/users.dto.ts
@@ -1,10 +1,17 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly rut: string;
 }
 
-export class UpdateUserDto {
-  readonly name?: string;
-  readonly lastName?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
